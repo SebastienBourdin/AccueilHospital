321eacb9f45e6fddf2c0f23f179d9b80dd9871de
walid <walidos195@gmail.com>
2017-05-08 17:52:09 +0200

    Update Rdvmdl.java

--- src/com/hopital/model/Rdvmdl.java
+++ src/com/hopital/model/Rdvmdl.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import com.hopital.domain.Rdv;
 
-
 public interface Rdvmdl {
 	public void saveRdv(Rdv rdv);
+
 	public List<Rdv> listRdv();
+
 	public List<Rdv> listRdv(Rdv rd);
+
 	List<Rdv> listCustom(Rdv rd);
+
 	public void updateRdv(Rdv rdv);
+
 	public void delete(String get);
 }
