105f126e40ab17416f1d6a1131d160e7aea4acb5
walid <walidos195@gmail.com>
2017-05-08 17:52:37 +0200

    Update DateUtil.java

--- src/com/hopital/utility/DateUtil.java
+++ src/com/hopital/utility/DateUtil.java
@@ -1,103 +1,101 @@
 package com.hopital.utility;
+
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-public class DateUtil {
-	public static Date getDate(String date){
 
-		
+public class DateUtil {
+
+	// date(String)=>date(Date)
+	public static Date getDate(String date) {
+
 		String[] v;
-		int year=0000;
-		int month=00;
-		int day=00;
-		if(!date.equals("")){
-			
-		if(date.contains("/")){
-		v=date.split("/");
+		int year = 0000;
+		int month = 00;
+		int day = 00;
+		// date n'est pas vide
+		if (!date.equals("")) {
+			// si date : dd/mm/yyyy
+			if (date.contains("/")) {
+				v = date.split("/");
+			}
+			// si date : dd-mm-yyyy
+			else {
+				v = date.split("-");
+			}
+			for (int i = 0; i < v.length; i++) {
+				String string = v[i];
+				if (string.length() == 4) {
+					year = Integer.parseInt(string) - 1900;
+				} else if (i == 1) {
+					month = Integer.parseInt(string) - 1;
+				} else {
+					day = Integer.parseInt(string);
+				}
+			}
 		}
-		else{
-		v=date.split("-");
-		}
-		for (int i = 0; i < v.length; i++) {
-			String string = v[i];
-			if(string.length()==4){
-				year=Integer.parseInt(string)-1900;
-			}
-			else if(i==1){
-				month=Integer.parseInt(string)-1;
-			}
-			else{
-				day=Integer.parseInt(string);
-			}
-		}}
-	Date date1=new Date(year, month, day);
+		Date date1 = new Date(year, month, day);
 		return date1;
 	}
 
-	public static String getDateS(String date){
+	public static String getDateS(String date) {
 
-		
 		String[] v;
-		String dates="";
-		if(!date.equals("")){
-			
-		if(date.contains("/")){
-		v=date.split("/");
-		}
-		else{
-		v=date.split("-");
-		}
-		for (int i =v.length-1; i>= 0; i--) {
-			
-			 if(i==2){
-				dates=dates+"20"+ v[i];
+		String dates = "";
+		if (!date.equals("")) {
+
+			if (date.contains("/")) {
+				v = date.split("/");
+			} else {
+				v = date.split("-");
 			}
-			 else{
-				 if(v[i].length()==1){
-					 dates+="-0"+v[i];
-				 }else{
-					 dates+="-"+v[i];
-				 }
-			 }
-			 
-			
-		}}
+			for (int i = v.length - 1; i >= 0; i--) {
+
+				if (i == 2) {
+					dates = dates + "20" + v[i];
+				} else {
+					if (v[i].length() == 1) {
+						dates += "-0" + v[i];
+					} else {
+						dates += "-" + v[i];
+					}
+				}
+
+			}
+		}
 		return dates;
 	}
 
-
-
-
-
-public static Date getRDate(String date){
-	String[] v;
-	int year=0000;
-	int month=00;
-	int day=00;
-	v=date.split("-");
-	for (int i = 0; i < v.length; i++) {
-		String string = v[i];
-		if(string.length()==4){
-			year=Integer.parseInt(string)+1900;
+	public static Date getRDate(String date) {
+		String[] v;
+		int year = 0000;
+		int month = 00;
+		int day = 00;
+		v = date.split("-");
+		for (int i = 0; i < v.length; i++) {
+			String string = v[i];
+			if (string.length() == 4) {
+				year = Integer.parseInt(string) + 1900;
+			} else if (i == 1) {
+				month = Integer.parseInt(string) + 1;
+			} else {
+				day = Integer.parseInt(string);
+			}
 		}
-		else if(i==1){
-			month=Integer.parseInt(string)+1;
-		}
-		else{
-			day=Integer.parseInt(string);
-		}
+		Date date1 = new Date(year, month, day);
+		return date1;
 	}
-	Date date1=new Date(year, month, day);
-	return date1;
-}
-public static boolean isValidDate(String inDate) {
-    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    dateFormat.setLenient(false);
-    try {
-      dateFormat.parse(inDate.trim());
-    } catch (ParseException pe) {
-      return false;
-    }
-    return true;
-  }
+
+	// verifier si inDate est bien formaté (yyyy-mm-dd)
+
+	public static boolean isValidDate(String inDate) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		dateFormat.setLenient(false);
+		try {
+			dateFormat.parse(inDate.trim());
+		} catch (ParseException pe) {
+			return false;
+		}
+		return true;
+	}
 }
